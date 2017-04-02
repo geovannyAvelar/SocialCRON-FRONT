823639509fe52dc9c0f0984b93552ddeeefbb23f
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-04-02 12:01:15 -0300

    Initial repository configuration

---
+++ Gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+ 
+var jsFilesToMove = [
+  'bower_components/angular/angular.min.js',
+  'bower_components/angular-cookies/angular-cookies.min.js',
+  'bower_components/angular-route/angular-route.min.js',
+  'bower_components/angular-ui-calendar/src/calendar.js',
+  'bower_components/fullcalendar/fullcalendar.min.js',
+  'bower_components/jquery/dist/jquery.min.js',
+  'bower_components/fullcalendar/dist/fullcalendar.min.js',
+  'bower_components/moment/fullcalendar.min.js',
+  'bower_components/materialize/dist/js/materialize.min.js',
+  'bower_components/moment/min/moment-with-locales.min.js',
+  'bower_components/mustache/mustache.min.js'
+];
+ 
+var cssFilesToMove = [
+  'bower_components/fullcalendar/dist/fullcalendar.min.css',
+  'bower_components/materialize/dist/css/materialize.min.css'
+];
+ 
+var fontsFilesToMove = [
+  'bower_components/materialize/dist/fonts/**/*'
+];
+ 
+
+gulp.task('assets-dist', function() {
+    gulp.src(jsFilesToMove)
+        .pipe(gulp.dest('assets/components/js'));
+    gulp.src(cssFilesToMove)
+        .pipe(gulp.dest('assets/components/css'));
+    gulp.src(fontsFilesToMove)
+        .pipe(gulp.dest('assets/components/fonts'));
+});
+ 
+gulp.task('default', [ 'assets-dist' ]);
---
+++ README.md
@@ -0,0 +1,33 @@
+# SocialCRON-FRONT
+SocialCRON is an open-source platform to schedule posts on social media sites
+
+## Summary
+* 1 Build
+    * 1.1 Pre-requisites
+    * 1.2 Build
+* 2 License
+
+## 1 Build
+
+### 1.1 Pre-requisites
+
+- [Bower](https://bower.io/)
+- [Gulp](http://gulpjs.com/)
+- [Git](https://git-scm.com/)
+
+### 1.2 Build
+
+1. Get the source typing the command:
+**git clone https://github.com/geovannyAvelar/SocialCRON-FRONT.git**
+
+2. Enter on project directory:
+**cd SocialCRON-FRONT**
+
+3. With Bower, install the dependencies:
+**bower install**
+
+4. Move the dependencies to assets/components directory with Gulp:
+**gulp**
+
+## 2 License
+This project is under The MIT License (MIT) terms.
