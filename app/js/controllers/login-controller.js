be47cfa2a9b0f73e41ef3b2eca019acb4308cc29
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-04-29 09:28:34 -0300

    Spinner o login form

--- app/js/controllers/login-controller.js
+++ app/js/controllers/login-controller.js
@@ -1,11 +1,17 @@
 app.controller("loginController", function($scope, $rootScope, $cookies, $location, AuthService) {
   $scope.credential = {"username": "", "password": "", "grant_type": "password"};
- 
+
   $scope.login = function() {
+    $scope.loading = true;
+
     AuthService
       .login($scope.credential)
         .then(function success(token) {
           $location.path('/newPost');
+          $scope.loading = false;
+        }, function error() {
+          $scope.error = "Invalid username or password. Try again.";
+          $scope.loading = false;
         });
   };
  
