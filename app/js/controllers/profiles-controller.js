47e5f609125e941f63a5c7213242fd558045320f
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-05-09 08:47:02 -0300

    Profiles deleting

--- app/js/controllers/profiles-controller.js
+++ app/js/controllers/profiles-controller.js
@@ -14,10 +14,31 @@ app.controller('profilesController', function($scope, ProfilesService) {
             $scope.profiles[i].expires = moment(profile.expires).format("LLLL"); 
           }
 
+          $('.modal').modal(); //Setting up the modals
+
           $scope.loading = false;
         }, function error() {
           $scope.loading = false;
         });
+  };
+
+  $scope.deleteProfile = function() {
+    if($scope.profiles.length > 0 && $scope.selectedProfile !== undefined) {
+      ProfilesService
+        .delete($scope.selectedProfile)
+          .then(function success(response) {
+            Materialize.toast('Profile has been deleted', 3000);
+            $scope.findAll();
+          }, function error() {
+            Materialize.toast('Cannot delete profile. Server error', 5000);
+          });
+    } else {
+      Materialize.toast('You need to select a profile before delete', 5000);
+    }
+  };
+
+  $scope.setSelectedProfile = function(id) {
+    $scope.selectedProfile = id;
   };
 
   $scope.findAll();
